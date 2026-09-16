39ac094acfa9b67c7b91f2e0ac6bf7ab3e192534
alidd11 <alicanimir1@gmail.com>
2026-09-16 23:28:35 +0100

    Fix location access: update docs/sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'rider-comms-pwa-v66';
+const CACHE_NAME = 'rider-comms-pwa-v67';
 const ASSETS = [
-  './', './index.html', './app.css?v=57', './app.js?v=60', './routes.css?v=31', './routes.js?v=31', './config.js?v=31',
+  './', './index.html', './app.css?v=58', './app.js?v=61', './routes.css?v=31', './routes.js?v=31', './config.js?v=31',
   './manifest.json', './icons/icon-192.png', './icons/icon-512.png',
 ];
 
